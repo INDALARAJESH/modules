b79b7d59e1373624e88a006cf7416ac87ca58eed
INDALARAJESH <rajeshindala1997@gmail.com>
2024-06-09 10:57:13 +0530

    added somefiles

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,24 @@
 resource "aws_instance" "ec2" {
-  ami                    = var.ami_id
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   tags = {
     Name = "ec2"
   }
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
